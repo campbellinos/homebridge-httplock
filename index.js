b001d98dd88f3dfec7489eb90006b3ea3c49e14e
campbellinos <campbellinos@gmail.com>
2016-03-19 17:41:35 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,83 @@
+var request = require("request");
+var Service, Characteristic;
+
+module.exports = function(homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+
+  homebridge.registerAccessory("homebridge-httplock", "Httplock", LockAccessory);
+}
+
+function LockAccessory(log, config) {
+  this.log = log;
+  this.name = config["name"];
+  this.lockID = config["lock_id"];
+  this.un = config["username"];
+  this.pw = config["password"];
+  
+  this.service = new Service.LockMechanism(this.name);
+  
+  this.service
+    .getCharacteristic(Characteristic.LockCurrentState)
+    .on('get', this.getState.bind(this));
+  
+  this.service
+    .getCharacteristic(Characteristic.LockTargetState)
+    .on('get', this.getState.bind(this))
+    .on('set', this.setState.bind(this));
+}
+
+LockAccessory.prototype.getState = function(callback) {
+  this.log("Getting current state...");
+  
+  request.get({
+    url: "https://campbellinos.us:5000/Locks",
+    qs: { un: this.un, pw: this.pw }
+  }, function(err, response, body) {
+    
+    if (!err && response.statusCode == 200) {
+      var json = JSON.parse(body);
+      var state = json.state; // "locked" or "unlocked"
+      this.log("Lock state is %s", state);
+      var locked = state == "locked"
+      callback(null, locked); // success
+    }
+    else {
+      this.log("Error getting state (status code %s): %s", response.statusCode, err);
+      callback(err);
+    }
+  }.bind(this));
+}
+
+LockAccessory.prototype.setState = function(state, callback) {
+  var lockState = (state == Characteristic.LockTargetState.SECURED) ? "locked" : "unlocked";
+
+  this.log("Set state to %s", lockState);
+
+  request.put({
+    url: "https://campbellinos.us:5000/Locks",
+    qs: { un: this.un, pw: this.pw, state: lockState }
+  }, function(err, response, body) {
+
+    if (!err && response.statusCode == 200) {
+      this.log("State change complete.");
+      
+      // we succeeded, so update the "current" state as well
+      var currentState = (state == Characteristic.LockTargetState.SECURED) ?
+        Characteristic.LockCurrentState.SECURED : Characteristic.LockCurrentState.UNSECURED;
+      
+      this.service
+        .setCharacteristic(Characteristic.LockCurrentState, currentState);
+      
+      callback(null); // success
+    }
+    else {
+      this.log("Error '%s' setting lock state. Response: %s", err, body);
+      callback(err || new Error("Error setting lock state."));
+    }
+  }.bind(this));
+},
+
+LockAccessory.prototype.getServices = function() {
+  return [this.service];
+}
